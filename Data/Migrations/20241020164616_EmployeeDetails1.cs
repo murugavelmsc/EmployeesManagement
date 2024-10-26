6d455bcb8461a14d80b2cbde98f7227857c5233c
Murugavel Sadagopan <murugavel.sadagopan@gmail.com>
2024-10-26 18:00:46 +0530

    Add project files.

---
+++ Data/Migrations/20241020164616_EmployeeDetails1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeesManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeDetails1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModifiedOnById",
+                table: "Employees",
+                newName: "ModifiedOn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModifiedOn",
+                table: "Employees",
+                newName: "ModifiedOnById");
+        }
+    }
+}
